9c20c6f91515e7366e4cf901acc37376158d16b2
Da Eul Jeong <elin.jeong@goodoc.co.kr>
2021-08-02 23:44:20 +0900

    etc : 패키지 이동

---
+++ src/programmer/level1/complete/BasicStringHandler.java
@@ -0,0 +1,50 @@
+package programmer.level1.complete;
+
+public class BasicStringHandler {
+    /*  문자열 s의 길이가 4 혹은 6이고,
+        숫자로만 구성돼있는지 확인해주는 함수, solution을 완성하세요.
+
+        예를 들어 s가 "a234"이면 False를 리턴하고 "1234"라면 True를 리턴하면 됩니다.
+
+        제한 사항
+            s는 길이 1 이상, 길이 8 이하인 문자열입니다.
+        입출력 예
+            s	return
+            "a234"	false
+            "1234"	true
+    */
+    public static void main(String[] args) {
+        boolean result = solution("a234");
+        if (result == false) {
+            System.out.println("1맞음");
+        }
+
+        result = solution("1234");
+        if (result == true) {
+            System.out.println("2맞음");
+        }
+
+        result = solution("0 1");
+        if (result == true) {
+            System.out.println("3맞음");
+        }
+
+        result = solution("0.1");
+        if (result == false) {
+            System.out.println("3맞음");
+        }
+
+    }
+
+    public static boolean solution(String s) {
+        try {
+            Integer.valueOf(s);
+        } catch (Exception e) {
+            return false;
+        }
+        if (s.length() == 4 || s.length() == 6) {
+            return true;
+        }
+        return false;
+    }
+}
---
+++ src/programmer/level1/complete/GetMiddleStringMain.java
@@ -0,0 +1,32 @@
+package programmer.level1.complete;
+
+public class GetMiddleStringMain {
+    public static void main(String[] args) {
+        String answer = solution("1234");
+        if(!answer.equalsIgnoreCase("23")){
+            System.out.println("1틀림");
+        }
+
+        answer = solution("12345");
+        if(!answer.equalsIgnoreCase("3")){
+            System.out.println("2틀림");
+        }
+
+        answer = solution("1");
+        if(!answer.equalsIgnoreCase("1")){
+            System.out.println("3틀림");
+        }
+        answer = solution("aaaaaaaaaabbbbbbbbbbaaaaaaaaaabbbbbbbbbbaaaaaaaaaabbbbbbbbbbaaaaaaaaaabbbbbbbbbbaaaaaaaaaabbbbbbbbbb");
+        System.out.println(answer);
+
+    }
+
+    private static String solution(String s) {
+        String answer = "";
+        int middleIndex = Math.round(s.length() / 2);
+        if(s.length() % 2 == 0){
+            return s.substring(middleIndex - 1, middleIndex +1);
+        }
+        return String.valueOf(s.charAt(middleIndex));
+    }
+}
